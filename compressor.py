96c6d524b13a9ef51cdd3eab67ae5f71cf8dcead
PapaZ810 <43076672+PapaZ810@users.noreply.github.com>
2021-11-30 00:13:06 -0700

    added another smaller picture and some code that doesn't work to extract the r value of each vector

--- compressor.py
+++ compressor.py
@@ -10,9 +10,19 @@ cos = math.cos
 file = sys.argv[len(sys.argv)-1]
 cwd = os.getcwd()
 arr = io.imread(cwd + "\\" + file)
+dim1 = len(arr)
+dim2 = len(arr[0])
+dim3 = len(arr[0][0])
 fourier = []
 for i in range(8):
     for j in range(8):
         fourier.append(cos((2*i+1)*j*pi/16))
-print(arr.size, arr)
 
+print(dim2)
+
+luminance = [ [None for y in range(dim2)] for x in range(dim1)]
+for i in range(dim2-1):
+    for j in range(dim1-1):
+            luminance[i][j] = arr[i][j][0]
+
+print(luminance)
