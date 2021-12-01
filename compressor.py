4a46baa703c61fa7357e5d9f4514ef1ab3ce7a40
PapaZ810 <43076672+PapaZ810@users.noreply.github.com>
2021-12-01 14:01:40 -0700

    added a fourier conversion algorithm that goes 8x8 chunks at a time

--- compressor.py
+++ compressor.py
@@ -1,21 +1,21 @@
 import numpy as np
-import imageio as io
-import os
-import sys
-import math
+import os, sys, math
+from PIL import Image
 
 pi = math.pi
 cos = math.cos
 
 file = sys.argv[len(sys.argv)-1]
 cwd = os.getcwd()
-arr = io.imread(cwd + "\\" + file)
+arr = Image.open(cwd + "\\" + file).convert('YCbCr')
+arr = np.array(arr)
 dim1 = len(arr)
 dim2 = len(arr[0])
-fourier = []
+
+fourier = np.empty(64).reshape(8, 8)
 for i in range(8):
     for j in range(8):
-        fourier.append(cos((2*i+1)*j*pi/16))
+        fourier[j][i] = (cos((2*i+1)*j*pi/16))
 
 print(dim2)
 
@@ -27,8 +27,19 @@ def extractor(dim1, dim2, array, vecnum):
 
     return result
 
-red = extractor(dim1, dim2, arr, 0)
-green = extractor(dim1, dim2, arr, 1)
-blue = extractor(dim1, dim2, arr, 2)
+luminance = extractor(dim1, dim2, arr, 0)
+Cr = extractor(dim1, dim2, arr, 1)
+Cb = extractor(dim1, dim2, arr, 2)
 
-print(red), print(green), print(blue), print(arr[dim1-2][dim2-2])
+print(luminance), print(Cr), print(Cb)
+
+fourierinv = np.linalg.inv(fourier)
+
+def fourierconversion(finv, array):
+    count = 0
+    for i in range(len(array) % 8)
+        for j in range(len(array[0] % 8))
+            result = finv * array[count:j % 8 * 8][count:i % 8 * 8]
+            count++
+    return result
+
