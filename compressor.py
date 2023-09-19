25dba7830395766f69fb99095818ef05fedad11f
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2023-09-18 21:07:49 -0600

    removed garbage

--- compressor.py
+++ compressor.py
@@ -1,12 +1,7 @@
 import threading
-
 from PIL import Image
-<<<<<<< Updated upstream
-import threading
-=======
 import math
 import sys
->>>>>>> Stashed changes
 import timeit
 import numpy as np
 
@@ -21,33 +16,13 @@ arr = np.array(image)
 x = len(arr)
 y = len(arr[0])
 
-<<<<<<< Updated upstream
 fourier = np.empty(64).reshape(8, 8)
 for i in range(8):
     for j in range(8):
-        fourier[i][j] = (cos((2*i+1)*j*pi/16))
-
-resultList = [np.unsignedinteger] * 3
-
-def extractor(array, vecnum, list):
-    dim1, dim2 = len(array), len(array[0])
-    result = np.empty(dim1*dim2).reshape(dim1, dim2)
-    for i in range(dim1):
-        for j in range(dim2):
-            result[i][j] = array[i][j][vecnum]
-    list[vecnum] = result
-
-t1 = threading.Thread(target = extractor, args=(arr, 0, resultList))
-t2 = threading.Thread(target = extractor, args=(arr, 1, resultList))
-t3 = threading.Thread(target = extractor, args=(arr, 2, resultList))
-
-t1.start(), t2.start(), t3.start()
-t1.join(), t2.join(), t3.join()
+        fourier[i][j] = (COS((2*i+1)*j*PI/16))
 
 fourierinv = np.linalg.inv(fourier)
 
-def fourierconversion(finv, array):
-=======
 global topLeft
 global topRight
 global bottomLeft
@@ -62,13 +37,12 @@ fourierInv = np.linalg.inv(fourier)
 
 
 def fourierConversion(finv, array):
->>>>>>> Stashed changes
     dim1, dim2 = len(array), len(array[0])
     result = np.empty(dim1*dim2).reshape(dim1, dim2)
     for i in range(int(len(array) / 8)):
         for j in range(int(len(array[i]) / 8)):
             result[i:i+8, j:j+8] = (finv * array[i:i+8, j:j+8])
-    return array
+    return result
 
 
 def clearBadValues(array, quality):
@@ -79,30 +53,6 @@ def clearBadValues(array, quality):
                 array[i][j] = 0
     return array
 
-<<<<<<< Updated upstream
-fourLum = fourierconversion(fourierinv, resultList[0])
-clearedLum = clearBadValues(fourLum, quality)
-resultLum = fourierconversion(fourier, clearedLum)
-
-fourCb = fourierconversion(fourierinv, resultList[1])
-clearedCb = clearBadValues(fourCb, quality)
-resultCb = fourierconversion(fourier, clearedCb)
-
-fourCr = fourierconversion(fourierinv, resultList[2])
-clearedCr = clearBadValues(fourCr, quality)
-resultCr = fourierconversion(fourier, clearedCr)
-
-def integrator(vec1, vec2, vec3):
-    dim1, dim2 = len(vec1), len(vec1[0])
-    result = np.empty(dim1*dim2*3, dtype=np.uint8).reshape(dim1, dim2, 3)
-    for i in range(dim1):
-        for j in range(dim2):
-            result[i][j][0], result[i][j][1], result[i][j][2] = vec1[i][j], vec2[i][j], vec3[i][j]
-    return result
-
-stop = timeit.default_timer()
-print('Time: ', stop - start)
-=======
 
 topLeft = arr[:int(x/2), :int(y/2)]
 topRight = arr[int(x/2):x, :int(y/2)]
@@ -153,4 +103,3 @@ Image.fromarray(resultArr, "YCbCr").save("compressed_" + file)
 
 stop = timeit.default_timer()
 print("Time: ", stop - start)
->>>>>>> Stashed changes
