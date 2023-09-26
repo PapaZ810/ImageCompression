205ef9dac77f184dcd815bd4191a046188194df0
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2023-09-26 15:24:21 -0600

    8 threaded image compressor
    Similar speed to the dual threaded compressor and similar CPU usage, but it is still slower than the single threaded one.

--- compressor.py
+++ compressor.py
@@ -25,8 +25,12 @@ fourierinv = np.linalg.inv(fourier)
 
 global topLeft
 global topRight
+global topMidLeft
+global topMidRight
 global bottomLeft
 global bottomRight
+global bottomMidLeft
+global bottomMidRight
 
 fourier = np.empty(64).reshape(8, 8)
 for i in range(8):
@@ -54,10 +58,14 @@ def clearBadValues(array, quality):
     return array
 
 
-topLeft = arr[:int(x/2), :int(y/2)]
-topRight = arr[int(x/2):x, :int(y/2)]
-bottomLeft = arr[:int(x/2), int(y/2):]
-bottomRight = arr[int(x/2):, int(y/2):]
+topLeft = arr[:int(x/4), :int(y/2)]
+topMidLeft = arr[int(x/4):int(x/2), :int(y/2)]
+topMidRight = arr[int(x/2):int((x/4)*3), :int(y/2)]
+topRight = arr[int((x/4)*3):, :int(y/2)]
+bottomLeft = arr[:int(x/4), int(y/2):]
+bottomMidLeft = arr[int(x/4):int(x/2), int(y/2):]
+bottomMidRight = arr[int(x/2):int((x/4)*3), int(y/2):]
+bottomRight = arr[int((x/4)*3):, int(y/2):]
 
 
 def compression(array: np.array):
@@ -73,6 +81,10 @@ def compression(array: np.array):
     global topRight
     global bottomLeft
     global bottomRight
+    global topMidRight
+    global topMidLeft
+    global bottomMidLeft
+    global bottomMidRight
 
     if threading.current_thread().name == "tl":
         topLeft = np.dstack((luminance, Cb, Cr))
@@ -80,6 +92,14 @@ def compression(array: np.array):
         topRight = np.dstack((luminance, Cb, Cr))
     elif threading.current_thread().name == "bl":
         bottomLeft = np.dstack((luminance, Cb, Cr))
+    elif threading.current_thread().name == "tml":
+        topMidLeft = np.dstack((luminance, Cb, Cr))
+    elif threading.current_thread().name == "tmr":
+        topMidRight = np.dstack((luminance, Cb, Cr))
+    elif threading.current_thread().name == "bml":
+        bottomMidLeft = np.dstack((luminance, Cb, Cr))
+    elif threading.current_thread().name == "bmr":
+        bottomMidRight = np.dstack((luminance, Cb, Cr))
     else:
         bottomRight = np.dstack((luminance, Cb, Cr))
 
@@ -88,14 +108,21 @@ t1 = threading.Thread(target=compression, args=(topLeft,), name="tl")
 t2 = threading.Thread(target=compression, args=(topRight,), name="tr")
 t3 = threading.Thread(target=compression, args=(bottomLeft,), name="bl")
 t4 = threading.Thread(target=compression, args=(bottomRight,), name="br")
+t5 = threading.Thread(target=compression, args=(topMidLeft,), name="tml")
+t6 = threading.Thread(target=compression, args=(topMidRight,), name="tmr")
+t7 = threading.Thread(target=compression, args=(bottomMidLeft,), name="bml")
+t8 = threading.Thread(target=compression, args=(bottomMidRight,), name="bmr")
 
-t1.start(), t2.start(), t3.start(), t4.start()
+t1.start(), t2.start(), t3.start(), t4.start(), t5.start(), t6.start(), t7.start(), t8.start()
 
-t1.join(), t2.join(), t3.join(), t4.join()
+t1.join(), t2.join(), t3.join(), t4.join(), t5.join(), t6.join(), t7.join(), t8.join()
 
-
-left = np.concatenate((topLeft, bottomLeft), axis=1)
-right = np.concatenate((topRight, bottomRight), axis=1)
+left1 = np.concatenate((topLeft, topMidLeft), axis=0)
+left2 = np.concatenate((bottomLeft, bottomMidLeft), axis=0)
+left = np.concatenate((left1, left2), axis=1)
+right1 = np.concatenate((topMidRight, topRight), axis=0)
+right2 = np.concatenate((bottomMidRight, bottomRight), axis=0)
+right = np.concatenate((right1, right2), axis=1)
 resultArr = np.concatenate((left, right), axis=0)
 
 
