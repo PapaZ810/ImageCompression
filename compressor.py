d5eb57875f853e5973085ab6fbf6233e88df804e
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2023-09-29 12:53:21 -0600

    working
    Got it back to a point where it works, just not super proud of it. Need to do more research into the fourier conversion itself.

--- compressor.py
+++ compressor.py
@@ -7,9 +7,7 @@ import timeit
 start = timeit.default_timer()
 
 pi = math.pi
-cos = math.cos
-np.set_printoptions(threshold=sys.maxsize, linewidth=1000)
-
+cos = np.cos
 file = sys.argv[-1].strip()
 quality = 2
 
@@ -18,10 +16,11 @@ arr = np.array(image)
 
 
 def compression(arr):
-    fourier = np.empty(64).reshape(8, 8)
+    fourier = np.empty((8, 8))
     for i in range(8):
         for j in range(8):
-            fourier[i][j] = (cos((2*i+1)*j*pi/16))
+            fourier[i][j] = (cos(((2*i+1)*j*pi)/16))
+
 
     fourierinv = np.linalg.inv(fourier)
     
@@ -29,21 +28,21 @@ def compression(arr):
     Cb = arr[:-1, :-1, 1]
     Cr = arr[:-1, :-1, 2]
 
-    luminance = fourierconversion(fourier, clearBadValues(fourierconversion(fourierinv, luminance), quality))
-    Cb = fourierconversion(fourier, clearBadValues(fourierconversion(fourierinv, Cb), quality))
-    Cr = fourierconversion(fourier, clearBadValues(fourierconversion(fourierinv, Cr), quality))
+    luminance = fourierconversion(fourierinv, clearBadValues(fourierconversion(fourier, luminance), quality))
+    Cb = fourierconversion(fourierinv, clearBadValues(fourierconversion(fourier, Cb), quality))
+    Cr = fourierconversion(fourierinv, clearBadValues(fourierconversion(fourier, Cr), quality))
 
     resultArr = np.dstack((luminance, Cb, Cr))
 
     Image.fromarray(resultArr, "YCbCr").save("compressed_" + file)
 
 
-def fourierconversion(finv, array):
-    dim1, dim2 = len(array), len(array[0])
-    result = np.zeros((dim1, dim2))
+def fourierconversion(four, array):
+    dim1, dim2 = array.shape
+    result = np.zeros_like(array)
     for i in range(int(dim1/8)):
         for j in range(int(dim2/8)):
-            result[i:i+8, j:j+8] = (finv * array[i:i+8, j:j+8])
+            result[8*i:8*i+8, 8*j:8*j+8] = np.dot(four, array[8*i:8*i+8, 8*j:8*j+8])
     return array
 
 
